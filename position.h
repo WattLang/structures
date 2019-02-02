7b230022161e3eef30aab55ee787cb79748cd4e9
Jack Clarke <me@jackclarke.xyz>
2019-02-02 02:24:48 +0000

    remove token (#2)

---
+++ position.h
@@ -0,0 +1,34 @@
+#pragma once
+
+
+#include <iostream>
+#include <cstdint>
+#include <string_view>
+
+
+namespace ws::token {
+    struct Position {
+        using size_type = std::uint32_t;
+
+
+        size_type line = 1, column = 1;
+
+        void nextln(size_type n = 1) {
+            column = 1;
+            line += n;
+        }
+
+        void next(size_type n = 1) {
+            column += n;
+        }
+    };
+
+
+    inline std::ostream& operator<<(std::ostream& os, const Position& tok) {
+        const auto& [line, column] = tok;
+
+        os << "(" << line << ", " << column << ")";
+
+        return os;
+    }
+}
